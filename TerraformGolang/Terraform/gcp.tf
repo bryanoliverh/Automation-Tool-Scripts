04256226691b8f9b0de38b1939d99204bd35f840
Bryan Oliver <74172600+bryanoliverh@users.noreply.github.com>
2023-05-03 16:25:02 +0800

    Delete TerraformGolang directory

--- TerraformGolang/Terraform/gcp.tf
+++
@@ -1,8 +0,0 @@
-provider "google" {
-  project = "my-project-id"
-  region  = "us-central1"
-  zone    = "us-central1-a"
-}
-
-resource "google_compute_instance" "instance" {
-  name         = var.instance_name
